f0e4f9edc7c6008bbdffa6942e4a7a97e6b75399
Don Yoon <dongho.yoon@gmail.com>
2022-05-31 16:42:52 -0700

    changing drivers

--- src/App.js
+++ src/App.js
@@ -7,34 +7,70 @@ class App extends Component {
     super(props)
     this.state = {
       squares: [null, null, null, null, null, null, null, null, null],
-      turn: [0]
+      turn: true,
+      whoWon: null,
+          
     }
   }
     
-
+    
 
     handleGamePlay = (index, whoseTurn) => {
       const {squares, turn} = this.state
-      turn[0] = whoseTurn[0] + 1
-      if(turn[0] %2 !== 0) {
+      
+      if (squares[index] !== null) {
+        void(0)
+      } else if(turn === true) {
         squares[index] = "⭕️"
-        this.setState({squares: squares, turn: turn})
-      } else {
+       
+        this.setState({squares: squares, turn: false})
+      } else if(turn === false) {
         squares[index] = "❌"
-        this.setState({squares: squares, turn: turn})
+        
+        this.setState({squares: squares, turn: true})
       }
       
-      
     }
+
+    winner (squareArr) {
+      
+      const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
+      let winner = null
+      for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squareArr[a] && squareArr[a] === squareArr[b] && squareArr[a] === squareArr[c]) {
+          winner = squareArr[a] ;
+        }
+      }
+      this.setState({whoWon: winner}) 
+    }
+  
+
   render() {
     console.log("square:", this.state.squares)
     console.log("turn:", this.state.turn)
+    console.log("playA:", this.state.playA)
+    console.log("playB:", this.state.playB)
+    console.log("win:", this.state.whoWon)
+
+    
+
     return(
       <>
         <h1>Tic Tac Toe</h1>
         <div className='game-board'> 
 
         
+
         {this.state.squares.map((value, index) => {
           return (
             <Square 
@@ -43,9 +79,13 @@ class App extends Component {
             index = {index}
             handleGamePlay = {this.handleGamePlay}
             turn = {this.state.turn}
+            win = {this.winner}
             /> 
           )
         })}
+        </div>
+        <div>
+          <p>{this.state.whoWon}</p>
         </div>
       </>
     )
